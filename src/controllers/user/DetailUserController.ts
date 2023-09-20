aa8ec34f103ad8d7c20d8bb9a3a95a5251d26a5f
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-09-20 12:01:50 -0300

    User details

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -5,11 +5,9 @@ class DetailUserController {
   async handle(request: Request, response: Response) {
     const user_id = request.user_id;
 
-    console.log(user_id);
-
     const detailUserService = new DetailUserService();
 
-    const detailUser = await detailUserService.execute();
+    const detailUser = await detailUserService.execute(user_id);
 
     return response.json(detailUser);
   }

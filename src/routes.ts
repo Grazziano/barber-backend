7d8904d2623107ef724b2fc5efea242bb63bcbbb
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-09-20 13:47:30 -0300

    Create haircut models

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { UpdateUserController } from './controllers/user/UpdateUserController';
+import { CreateHaircutController } from './controllers/haircut/CreateHaircutController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -13,5 +14,8 @@ router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
 router.put('/users', isAuthenticated, new UpdateUserController().handle);
+
+// ROTAS HAIRCUT
+router.post('/haircut', isAuthenticated, new CreateHaircutController().handle);
 
 export { router };
